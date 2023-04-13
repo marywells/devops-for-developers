2cb40e2d702d63c98fabcbd5d2b4212071976890
Mary Wells <m.a.wells@outlook.com>
2023-04-13 16:17:30 +0100

    autogenerating dockerfile

--- service-templates/index.ts
+++ service-templates/index.ts
@@ -1,20 +1,32 @@
 //@ts-nocheck
 import { readFileSync } from 'fs';
 import Yargs, { config } from 'yargs';
+import { configure, render } from 'nunjucks';
+
+type BuildJSONDockerFile = {
+  PreInstallCommands: string[];
+};
 
 type BuildJSON = {
+  Dockerfile: BuildJSONDockerFile;
   Name: string;
   Service: string;
 };
 
 function main() {
   const argv = Yargs(process.argv.slice(2)).argv;
-
   const buildPath = argv['config'];
 
   const configuration = readFileSync(buildPath, 'utf8');
   const configurationData = JSON.parse(configuration) as BuildJSON;
-  console.log(configurationData.Name);
+
+  configure('templates', { autoescape: true });
+
+  const file = render('Dockerfile', {
+    ServiceName: configurationData.ServiceName,
+    ServiceType: configurationData.ServiceType,
+  });
+  console.log(file);
 }
 
 main();
