76de936d4c8e4b86491cad2e995a77c13ab44753
Mary Wells <m.a.wells@outlook.com>
2023-04-13 15:53:50 +0100

    build CLI tool with yargs

--- service-templates/index.ts
+++ service-templates/index.ts
@@ -1,4 +1,20 @@
+//@ts-nocheck
+import { readFileSync } from 'fs';
+import Yargs, { config } from 'yargs';
+
 type BuildJSON = {
   Name: string;
   Service: string;
 };
+
+function main() {
+  const argv = Yargs(process.argv.slice(2)).argv;
+
+  const buildPath = argv['config'];
+
+  const configuration = readFileSync(buildPath, 'utf8');
+  const configurationData = JSON.parse(configuration) as BuildJSON;
+  console.log(configurationData.Name);
+}
+
+main();
